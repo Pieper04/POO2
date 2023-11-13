4d1e4f5bae75f66471b10750fb4efc7a37934d92
Letícia Pieper <leticia.pieper1@gmail.com>
2023-11-13 11:06:14 -0400

    Add files via upload

--- 13-11/Ex01/Program.java
+++ 13-11/Ex01/Program.java
@@ -1,1 +1,17 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse.BodyHandlers;
 
+public class Program {
+    public static void main(String... args) throws IOException, InterruptedException {
+        final var client = HttpClient.newHttpClient();
+
+        final var request = HttpRequest.newBuilder(URI.create("https://viacep.com.br/ws/78557730/json/")).build();
+
+        final var result = client.send(request, BodyHandlers.ofString());
+
+        System.out.println(result.body());
+    }
+}
